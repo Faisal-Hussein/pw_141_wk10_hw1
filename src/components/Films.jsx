e340f3ae232b42c9c518935a55f4eb8cb364800a
Faisal-Hussein <fh069017@ohio.edu>
2024-04-24 16:32:05 -0400

    Communicating with films portion of flask API

--- src/components/Films.jsx
+++ src/components/Films.jsx
@@ -1,28 +1,45 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Container } from 'react-bootstrap';
 
 export default function Films() {
 
+    const [films, setFilms] = useState([]);
+    console.log(films);
+    useEffect(() => {
+        (async () => {
+            await getFilms();
+        })()
+    }, [])
 
-    const films = [
-        "Friday the 13th", 'Spiderman: Into the Spiderverse ', 'Little Mermaid', 'Mario Movie', 'Barbie', 'Castle in the Sky'
-    ];
-
-    const [ filmSelected, setFilmSelected ] = useState('?');
-    console.log(filmSelected);
-
-    function getRandomFilm() {
-        return films[Math.floor(Math.random() * films.length)]
+    async function getFilms() {
+        const res = await fetch ('http://127.0.0.1:5000/film')
+        if (res.ok) { 
+            const data = await res.json();
+            console.log(data);
+            setFilms(data);
+        } else console.error("Failed to load posts")
     }
+    // const films = [
+    //     "Friday the 13th", 'Spiderman: Into the Spiderverse ', 'Little Mermaid', 'Mario Movie', 'Barbie', 'Castle in the Sky'
+    // ];
+
+    // const [ filmSelected, setFilmSelected ] = useState('?');
+    // console.log(filmSelected);
+
+    // function getRandomFilm() {
+    //     return films[Math.floor(Math.random() * films.length)]
+    // }
 
     return (
         <Container>
             <div>
-                <p>Film Choice: {filmSelected}</p>
+                <h2>Films</h2>
+
+                {/* <p>Film Choice: {filmSelected}</p>
                 <button onClick={()=>{
                     setFilmSelected( getRandomFilm() );
-                }}>Random Film!</button>
+                }}>Random Film!</button> */}
             </div>
         </Container>
     )
